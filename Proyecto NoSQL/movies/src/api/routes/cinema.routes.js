19e968e414198d19d9bb52f0cd87bf2752395083
AndreaContreras06 <andreacontreraslopez06@gmail.com>
2026-06-14 14:48:21 +0200

    Refactor cinema routes to use controller methods

--- Proyecto NoSQL/movies/src/api/routes/cinema.routes.js
+++ Proyecto NoSQL/movies/src/api/routes/cinema.routes.js
@@ -1,19 +1,9 @@
 const express = require("express");
-
-const Cinema = require("../models/cinema.model");
+const { getCinemas, postCinema } = require("../controllers/cinema.controller");
 
 const cinemaRoutes = express.Router();
 
-cinemaRoutes.get("/", async (req, res) => {
-  const cinemas = await Cinema.find().populate("movies");
-  res.json(cinemas);
-});
-
-cinemaRoutes.post("/", async (req, res) => {
-  const cinema = new Cinema(req.body);
-  const savedCinema = await cinema.save();
-
-  res.status(201).json(savedCinema);
-});
+cinemaRoutes.get("/", getCinemas);
+cinemaRoutes.post("/", postCinema);
 
 module.exports = cinemaRoutes;
