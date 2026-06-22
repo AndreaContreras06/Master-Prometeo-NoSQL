b5d31be01a551c89058e7e23a2c0ae845ceeda27
AndreaContreras06 <andreacontreraslopez06@gmail.com>
2026-06-22 19:30:11 +0200

    Add files via upload

---
+++ Movies/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const errorHandler = require('./src/utils/error');
+
+const movieRoutes = require("./src/api/routes/movie.routes");
+const cinemaRoutes = require("./src/api/routes/cinema.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use("/movies", movieRoutes);
+app.use("/cinemas", cinemaRoutes);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+    app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
+  })
+  .catch((err) => console.error("Error de conexión:", err));
