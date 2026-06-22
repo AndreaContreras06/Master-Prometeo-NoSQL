96cdb86cad126d53c588a00f4dc54e798fb50c6b
AndreaContreras06 <andreacontreraslopez06@gmail.com>
2026-06-22 19:21:32 +0200

    Delete Proyecto NoSQL/movies/src directory

--- Proyecto NoSQL/movies/src/api/routes/cinema.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getCinemas, postCinema } = require("../controllers/cinema.controller");
-
-const cinemaRoutes = express.Router();
-
-cinemaRoutes.get("/", getCinemas);
-cinemaRoutes.post("/", postCinema);
-
-module.exports = cinemaRoutes;
--- Proyecto NoSQL/movies/src/utils/error.js
+++
@@ -1,7 +0,0 @@
-const errorHandler = (error, req, res, next) => {
-  return res.status(500).json({
-    message: error.message
-  });
-};
-
-module.exports = errorHandler;
